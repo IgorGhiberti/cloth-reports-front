715600d6184b7977b0ad55ead54a766f57edb28c
Igor Ghiberti <igorgh@outlook.com>
2024-08-08 13:37:02 -0300

    Formulário corrigido

--- src/app/Componentes/Categoria/editar-categoria/editar-categoria.component.ts
+++ src/app/Componentes/Categoria/editar-categoria/editar-categoria.component.ts
@@ -3,29 +3,29 @@ import { CategoriaService } from '../../../Services/categoria.service';
 import { Categoria } from '../../../Models/categoria';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FormularioCategoriaComponent } from "../formulario-categoria/formulario-categoria.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-editar-categoria',
   standalone: true,
-  imports: [FormularioCategoriaComponent],
+  imports: [FormularioCategoriaComponent, CommonModule],
   templateUrl: './editar-categoria.component.html',
   styleUrl: './editar-categoria.component.scss'
 })
 export class EditarCategoriaComponent implements OnInit {
 
   btnTitle = 'Editar categoria';
+  btnAcao = 'Confirmar edição';
   categoria!: Categoria;
   idNumber!: number;
   constructor(private categoriaService: CategoriaService, private router: Router, private route: ActivatedRoute){}
 
   ngOnInit(): void {
     const id = Number (this.route.snapshot.paramMap.get('id'));
+    this.idNumber = id;
     this.categoriaService.getCategoriaById(id).subscribe(data => {
       this.categoria = data
-      console.log('data: ', this.categoria)
     });
-    this.idNumber = id;
-    console.log('id atual: ', id)
   }
 
   editarCategoria(categoria: Categoria)
--- src/app/Componentes/Categoria/formulario-categoria/formulario-categoria.component.ts
+++ src/app/Componentes/Categoria/formulario-categoria/formulario-categoria.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Router } from '@angular/router';
 import { CategoriaService } from '../../../Services/categoria.service';
 import { Categoria } from '../../../Models/categoria';
@@ -12,17 +12,20 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
   templateUrl: './formulario-categoria.component.html',
   styleUrl: './formulario-categoria.component.scss'
 })
-export class FormularioCategoriaComponent {
+export class FormularioCategoriaComponent implements OnInit {
   @Output() onSubmitFunction = new EventEmitter<Categoria>();
   @Input() btnTitulo!: string;
+  @Input() btnAcao!: string;
   @Input() dadosCategoria: Categoria | null = null;
-
   categoriaForm!: FormGroup;
 
   categoria!: Categoria;
-  constructor(private router: Router, private categoriaService: CategoriaService){
+  constructor(private router: Router, private categoriaService: CategoriaService){}
+
+  ngOnInit(): void {
+
     this.categoriaForm = new FormGroup({
-      id: new FormControl(this.dadosCategoria ? this.dadosCategoria.idcategoria: 0),
+      idcategoria: new FormControl(this.dadosCategoria ? this.dadosCategoria.idcategoria: 0),
       nome: new FormControl(this.dadosCategoria ? this.dadosCategoria.nome: '', [Validators.required])
     })
   }
